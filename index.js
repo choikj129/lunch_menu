0db4f97b996fef2cdd34c695c21e607d619dc9ae
choikj129 <ckj9829@naver.com>
2022-09-24 17:37:01 +0900

    feat : vue setting

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const vue = require("vue");
 const app = express();
 
+app.use(express.static(__dirname + '/views'));
+app.set("view engine", "html");
+
 app.get('/',(req,res) => {
-    res.send('Hello World!')
+    res.render("index")
 })
 
 app.listen(3000,()=>console.log('server start!'))
